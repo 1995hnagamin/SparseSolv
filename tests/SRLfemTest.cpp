3b5bd4935c6658b29e765fb696841c5d46e5d057
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2026-02-02 19:06:10 +0900

    add SRLfemTest.cpp

---
+++ tests/SRLfemTest.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include "SparseSolv/SparseMat.hpp"
+
+TEST(SRLfemTest, IsFixedAssertions) {
+  constexpr int size = 3;
+  double matA[size][size] ={
+    { 1.1, 1.2, 0.0 },
+    { 1.2, 2.2, 0.0 },
+    { 0.0, 0.0, 3.3 },
+  };
+
+  SRLfem::SparseMat matAs(size);
+  for(int i = 0 ; i < size ; i++){
+    for(int j = 0 ; j < size ; j++){
+      if( fabs(matA[i][j]) > 1.0e-12 ){
+        matAs.add(i, j, matA[i][j]);
+      }
+    }
+  }
+  EXPECT_EQ(matAs.isFixed(), false);
+  matAs.fix();
+  EXPECT_EQ(matAs.isFixed(), true);
+}
